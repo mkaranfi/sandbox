34ec27192a5f23732f6f28f99e0939303b03acc8
Mile Karanfilov <mkaranfi1733@gmail.com>
2025-01-28 15:06:16 +0100

    Add dev option to deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,34 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
-module.exports = {
-    entry: './index.js', // Entry point for your application
-    output: {
-        filename: 'bundle.js', // Output file name
-        path: path.resolve(__dirname, 'dist'), // Output directory
-    },
-    mode: 'production', // Set mode to 'development' or 'production'
-    devtool: 'source-map', // Enable source maps for easier debugging
-    module: {
-        rules: [
-            {
-                test: /\.js$/, // Apply this rule to all JavaScript files
-                exclude: /node_modules/, // Exclude node_modules folder
-                use: 'babel-loader', // Transpile JS files using Babel
-            },
+module.exports = (env, argv) => {
+    const isProduction = argv.mode === 'production'
+
+    return {
+        entry: './index.js', // Entry point for your application
+        output: {
+            filename: 'bundle.js', // Output file name
+            path: isProduction
+                ? path.resolve(__dirname, 'dist')  // Production: Output to 'dist'
+                : path.resolve(__dirname, 'dev') // Development: Output to 'dev'
+        },
+        mode: isProduction ? 'production' : 'development', // Set mode to 'development' or 'production'
+        devtool: 'source-map', // Enable source maps for easier debugging
+        module: {
+            rules: [
+                {
+                    test: /\.js$/, // Apply this rule to all JavaScript files
+                    exclude: /node_modules/, // Exclude node_modules folder
+                    use: 'babel-loader', // Transpile JS files using Babel
+                },
+            ],
+        },
+        plugins: [
+            // This will generate a new index.html and inject the JS file(s)
+            new HtmlWebpackPlugin({
+                template: './index.html', // Path to your index.html template
+                filename: 'index.html',       // Output file name (in dist folder)
+            }),
         ],
-    },
-    plugins: [
-        // This will generate a new index.html and inject the JS file(s)
-        new HtmlWebpackPlugin({
-            template: './index.html', // Path to your index.html template
-            filename: 'index.html',       // Output file name (in dist folder)
-        }),
-    ],
+    }
 };
